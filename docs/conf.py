20f06b66ada5914e9cebc955bc1da3a53d0ee8b4
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-01-31 13:05:47 +0000

    Added the path to the package

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # PackageName documentation build configuration file, created by
-# sphinx-quickstart on Tue Jan 31 12:31:42 2017.
+# sphinx-quickstart on Tue Jan 31 12:46:14 2017.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -16,9 +16,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 
